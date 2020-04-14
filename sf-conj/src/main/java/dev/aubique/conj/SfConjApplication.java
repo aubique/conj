ade839ff4f1279797f0fc2a5ecfa3026f6d06e06
aubique <ubicquita@gmail.com>
2020-04-14 23:12:00 +0200

    Initialize Spring Boot project

---
+++ sf-conj/src/main/java/dev/aubique/conj/SfConjApplication.java
@@ -0,0 +1,13 @@
+package dev.aubique.conj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfConjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfConjApplication.class, args);
+	}
+
+}
---
+++ sf-conj/src/test/java/dev/aubique/conj/SfConjApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aubique.conj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfConjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
